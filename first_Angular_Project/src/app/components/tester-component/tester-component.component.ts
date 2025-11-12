be8e5799d010b3d822902897e7eb7afe329b8c35
Talal <talalzahid912@gmail.com>
2025-11-12 20:10:10 +0500

    custom events with output and eventemitter...

--- first_Angular_Project/src/app/components/tester-component/tester-component.component.ts
+++ first_Angular_Project/src/app/components/tester-component/tester-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-tester-component',
@@ -15,5 +15,14 @@ export class TesterComponentComponent {
   transformText() {
     this.announcement = this.announcement.toUpperCase();
   }
-  
+
+  // Custom Event
+  @Input() greeting!: string ;
+
+  // Define a custom event
+  @Output() notify = new EventEmitter<string>();
+
+  sendMessageToParent() {
+    this.notify.emit('Hello Parent! Message from Child 👋');
+  }
 }
